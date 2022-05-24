913be947f13a690428a911415fb8b64ce5a202d5
JulianMozi <Jkoripamo@gmail.com>
2022-05-24 15:39:33 +0100

    Note buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -6,16 +7,86 @@ void main() {
 
 // ignore: use_key_in_widget_constructors
 class XylophoneApp extends StatelessWidget {
+  void playNote(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'My Xylophone',
       home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(title: const Text('My Xylophone'),),
+          appBar: AppBar(
+            title: const Text('My Xylophone'),
+          ),
           body: Column(
-            children: const [
-
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  playNote(1);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () {
+                  playNote(2);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () {
+                  playNote(3);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () {
+                  playNote(4);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  playNote(5);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.indigo,
+                ),
+                onPressed: () {
+                  playNote(6);
+                },
+                child: const Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                onPressed: () {
+                  playNote(7);
+                },
+                child: const Text(''),
+              ),
             ],
           ),
         ),
